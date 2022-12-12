9cd556241a0b2c1a50cf2183ed4be7fa6fcbc4d7
marianorigo18 <marianowagnerrigo@gmail.com>
2022-12-11 23:38:56 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+const colors = ["green", "red", "gray", "rgba(133, 122, 200)", "#f15025"];
+
+const button = document.querySelector('#btn');
+const color = document.querySelector('.color');
+
+button.addEventListener('click', function(){
+    const randomNumber = getRandomNumber();
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+    console.log(Math.floor(Math.random()* colors.length))
+});
+
+function getRandomNumber(){
+    return Math.floor(Math.random()*colors.length);
+};
